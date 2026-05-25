b7d2bf5a4b9fca1a12e996259739993bfcd4dbcd
Vishoo <vverma971@gmail.com>
2026-05-25 13:55:12 +0530

    async intro

---
+++ Async/1.js
@@ -0,0 +1,51 @@
+// console.log("Start");
+
+// // async 
+// setTimeout(()=>{
+//     console.log("Runs after 1 second")
+// }, 0);
+
+
+// console.log("End");
+
+
+
+// console.log("1");
+
+// setTimeout(() => {
+//   console.log("2");
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log("3");
+// }, 1000);
+
+// console.log("4");
+
+
+// console.log("X");
+
+// setTimeout(() => {
+//   console.log("Y");
+
+//   setTimeout(() => {
+//     console.log("Z");
+//   }, 1000);
+// }, 1000);
+
+// console.log("W");
+
+
+console.log("One");
+
+setTimeout(() => {
+  console.log("Two");
+}, 500);
+
+console.log("Three");
+
+setTimeout(() => {
+  console.log("Four");
+}, 0);
+
+console.log("Five");
