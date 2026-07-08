75138ad3111d69f7e09f70ba50e3e225e01263e0
Vishoo <vverma971@gmail.com>
2026-07-08 14:21:06 +0530

    products

--- ecommerce/src/components/FilterBar.jsx
+++ ecommerce/src/components/FilterBar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { useState } from 'react';
-const FilterBar = ({categories, category, filter}) => {
-    const categoriesArr = Array.from(categories)
-
-    const [selectedCategory, setSelectedCat] = useState("");
+const FilterBar = ({categories, category, setCategory}) => {
+    const categoriesArr = Array.from(categories);
+  console.log(categories)
   return (
     <div className= 'border'> 
+
+
+
     
     <select
-    value={selectedCategory}
-    onChange={(e)=>filter(e.target.value)}
+    value={category}
+    onChange={(e)=>setCategory(e.target.value)}
     >
-        <option value="All">All</option>
         {categoriesArr.map((category)=>{
 
             return <option key={category} value={category}>
--- ecommerce/src/components/Shop.jsx
+++ ecommerce/src/components/Shop.jsx
@@ -4,53 +4,113 @@ import FilterBar from "./FilterBar";
 
 const Shop = ()=>{
 const [products, setProducts] = useState([]);
-const [filtered, setFiltered] = useState([]);
-const [category, setCategory] = useState("");
-// dummyjson.com/products
+const [category, setCategory] = useState("All");
+const [price, setPrice] = useState(0);
+// const [categories, setCategories] = useState([]);
+    useEffect(()=>{
+try{
+        const getProducts = async ()=>{
+                const response= await fetch("https://dummyjson.com/products");
 
-// []=> <Product product={product} />
+                if(!response.ok){
+                    throw new Error("Products couldnt be fetched")
+                }
 
+                const data = await response.json();
 
-useEffect(()=>{
-    const getProducts = async () =>{
-      const response= await fetch("https://dummyjson.com/products");
+                console.log(data);
 
-      if(!response.ok){
-        return "No products found"
-      }
+                const productsData = data.products;
 
-      const data = await response.json();
-      const fetchedproducts = data.products;
-      setProducts(fetchedproducts)
-      setFiltered(fetchedproducts)
-      console.log(data);
+                
+                setProducts(productsData);
+                
+        }
+
+        getProducts();
     }
+    catch(err){
+        console.error(err.message)
+    }
+    }, [])
 
-    getProducts();
+    const categories = ["All", ...new Set(products.map((prod)=>prod.category))];
 
-}, [])
+    const filteredProducts = products.filter(
+        (prod)=>{
+            let matchCategory = true;
+            
+            if(category!=="All"){
+            if(prod.category!==category){
+                matchCategory=false;
+            }
 
-let categories = [...products.map((item)=>item.category)];
-categories = new Set(categories)
+
+        }
+
+            let matchPrice = true;
+
+            if(price!==0){
+                if(prod.price>price){
+                    matchPrice=false;
+                }
+            }
+
+
+            return matchPrice && matchCategory;
+
+        })
+                
     return (
         <div className="min-h-screen bg-gray-100 p-2">
-            <h1>This is the shop</h1>
-
-            <FilterBar 
-            categories={categories}
-            selectedCategory = {category}
-            filter = {setCategory}
-            />
+            <p className="text-black">This is the shop</p>
             <p>{category}</p>
-            <div className="grid gap-5 grid-cols-4">
-                {filtered.map((product)=>{
-                    if(product.category==category){
-                            return <ProductCard product={product} key={product.id} />
-                        }
-                }
-                )}
-                
+
+        <FilterBar 
+        setCategory={setCategory}
+        category={category}
+        categories={categories}
+
+/>
+
+
+            <p>below is the shop select tag and above is the filterbar</p>
+
+
+
+        <select
+        value={category}
+        onChange={(e)=>setCategory(e.target.value)}
+        >
+            {
+                categories.map((cat)=>{
+                    return <option key={cat} value={cat}>{cat}</option>
+                })
+            }
+        </select>
+
+        <div className="flex align-center justify-center">
+
+            <input
+            type='range'
+            min="0"
+            max="5000"
+            value={price}
+            onChange={(e)=>setPrice(e.target.value)}
+
+            />
+
+            <p>{price}</p>
+</div>
+
+            <div className="grid grid-cols-4 gap-2">
+            {
+                filteredProducts.map((prod)=>{
+                    return <ProductCard  key={prod.id} product = {prod} />
+                })
+            }
             </div>
+
         </div>
     )
 }
